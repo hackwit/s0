352f4a5809da6357ee79e3a80d1c355ec46c07db
Val <dgm97@cornell.edu>
2016-07-21 17:54:08 -0400

    soundcloud testing

--- js/script.js
+++ js/script.js
@@ -2,7 +2,30 @@
 	console.log("I'm here!");
 
 	// $('#datetime').innerHTML = Date();
+	var d = new Date();
+	document.getElementById('datetime').innerHTML = d.toDateString();
 
-	document.getElementById('datetime').innerHTML = Date();
+    var widgetIframe = document.getElementById('sc-widget');
+    var newSoundUrl = 'http://api.soundcloud.com/tracks/267065000';
+    widgetIframe.src = "https://w.soundcloud.com/player/?url=" + newSoundUrl;
+    var widget = SC.Widget(widgetIframe);
+
+    widget.bind(SC.Widget.Events.READY, function() {
+    	widget.load(newSoundUrl, {
+        	show_artwork: false,
+        	show_playcount: false,
+        	show_comments: false,
+        	buying: false,
+        	sharing: false,
+        	show_user: false
+        });
+      // load new widget
+      widget.bind(SC.Widget.Events.FINISH, function() {
+        widget.load(newSoundUrl, {
+        	show_artwork: false
+        });
+      });
+    });
+
 }(jQuery));
 	
